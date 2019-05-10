5e71cf38c2d393b1533f0e894cf6a47e50730f4c
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2019-05-10 13:55:20 +0200

    meep

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    Transform objectT;
+    private readonly string objectName = "Cube";
+    private Vector3 distanceObject, triggerRadius;
+    private readonly float movementSpeed = 3;
+    void Start()
+    {
+        objectT = GameObject.Find(objectName).GetComponent<Transform>();
+        triggerRadius = new Vector3(4, 4, 4);
+    }
+
+    void Update()
+    {
+        FaceTowardsAndWalkToObject();
+    }
+
+    Vector3 DistanceToObject()
+    {
+        Vector3 result = objectT.position - transform.position;
+        return result;
+    }
+
+    private void FaceTowardsAndWalkToObject()
+    {
+        distanceObject = DistanceToObject();
+        float pX = Mathf.Abs(DistanceToObject().x), pZ = Mathf.Abs(DistanceToObject().z);
+        if (pX < triggerRadius.x && pX > objectT.localScale.x || pZ < triggerRadius.z && pZ > objectT.localScale.z)
+        {
+            transform.rotation =  Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, distanceObject, 1, 0.0f));
+            transform.Translate(0, 0, movementSpeed * Time.deltaTime);
+        }
+        if (pX <= objectT.localScale.x && pZ <= objectT.localScale.z)
+        {
+            Attack();
+        } 
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+    }
+
+}
---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private readonly float movementSpeed = 5, rotSpeed = 50;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,0, movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotSpeed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
+        }
+    }
+}
